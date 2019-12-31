e500c9f8262595a16442a33338fc9d8bff9ca945
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-12-31 16:51:30 +0000

    Feature (html): Add code to generate html

--- src/gbooks.c
+++ src/gbooks.c
@@ -9,13 +9,52 @@
 
 #include <json-c/json.h>
 
-#include "books.h"
+//#include "books.h"
+
+void	set_html_header(FILE *fp);
+void	set_html_body_total(FILE *fp, int total);
+//void	set_html_body_volume(FILE *fp, Volume *volume);
+void	set_html_footer(FILE *fp);
 
 int
 main()
 {
-	init_curl();
-	
-	close_curl();
+	FILE *fp;
+
+	fp = fopen("test.html", "w");
+	set_html_header(fp);
+	set_html_body_total(fp, 10);
+	set_html_footer(fp);
+	fclose(fp);
 	return 0;
+}
+
+void set_html_header(FILE *fp)
+{
+	fprintf(fp, "<!DOCTYPE html>\n");
+	fprintf(fp, "<html lang=\"pt\">\n");
+	fprintf(fp, "<header>\n");
+	fprintf(fp, "<meta charset=\"utf-8\">\n");
+	fprintf(fp, "<title>Apresentacao de volumes</title>\n");
+	fprintf(fp, "</header>\n");
+	fprintf(fp, "<body>\n");
+}
+
+void
+set_html_body_total(FILE *fp, int total)
+{
+	fprintf(fp, "<table>\n");
+	fprintf(fp, "<tbody>\n");
+	fprintf(fp, "<tr><td colspan=2>\n");
+	fprintf(fp, "Total de volumes: %d\n", total);
+	fprintf(fp, "</td></tr>\n");
+}
+
+void
+set_html_footer(FILE *fp)
+{
+	fprintf(fp, "</tbody>\n");
+	fprintf(fp, "</table>\n");
+	fprintf(fp, "</body>\n");
+	fprintf(fp, "</html>");
 }
