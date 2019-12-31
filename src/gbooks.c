61da2f72a629baa9c2f888837d91ae2a6780ba48
Luis Bandarra <luis.bandarra@homestudio.pt>
2019-12-31 21:18:03 +0000

    Feature (app): add generation  html code

--- src/gbooks.c
+++ src/gbooks.c
@@ -5,28 +5,61 @@
  * All rights reserved.
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <dlfcn.h>
 
 #include <json-c/json.h>
 
 //#include "books.h"
 
+void	usage();
 void	set_html_header(FILE *fp);
 void	set_html_body_total(FILE *fp, int total);
 //void	set_html_body_volume(FILE *fp, Volume *volume);
 void	set_html_footer(FILE *fp);
 
 int
-main()
+main(int argc, char *argv[])
 {
 	FILE *fp;
+	char author[50];
+
+	char *apikey = getenv("GBOOKS_API_KEY");
+	if (apikey == NULL) {
+		usage();
+		return (-1);
+	}
+
+	if (argc != 2) {
+		usage();
+		return (-1);
+	}
+	strcpy((char *) author, argv[1]);
+	printf("Author: %s\n", author);
 
 	fp = fopen("test.html", "w");
+	if (fp == NULL) {
+		fprintf(stderr, "Failed to open file to write.");
+		return (-1);
+	}
+
 	set_html_header(fp);
 	set_html_body_total(fp, 10);
 	set_html_footer(fp);
 	fclose(fp);
 	return 0;
+}
+
+void
+usage()
+{
+	printf("Usage: gbooks autor\n");
+	printf("\t\ta variavel de ambiente GBOOKS_API_KEY.\n");
+	printf("autor\t\tnecessario para fazer a pesquisa. Pode ter espacos, \n");
+	printf("\t\tmas nesse caso devem colocar aspas para limitar a pesquisa.\n");
+	printf("\t\tDeve definir a variavel ambiente GBOOKS_API_KEY para um\n");
+	printf("\t\tvalor, exemplo: AIzaSyAdM5Km30Z3x7I9vvn15akWjLd9lBnwVpU\n");
 }
 
 void set_html_header(FILE *fp)
